be57d70c5e607f83973e1211172071d435947644
Drew Vollrath <andre@vollrath.ca>
2017-01-01 09:02:59 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,10 @@
 require 'pry'
 
 def my_find(collection)
-
+  count = 0
+  while count < collection.length
+    #return collection[count] if yield(collection[count]) == TrueClass
+    return collection[count] if yield(collection[count])
+    count += 1
+  end
 end
