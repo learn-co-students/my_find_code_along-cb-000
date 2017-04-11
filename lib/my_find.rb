9f593ca4cd55de782686f79e0baca147a7fee35e
Matthew McAnelly <mail@matthewmcanelly.com>
2017-04-11 14:22:36 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,19 @@
 require 'pry'
 
 def my_find(collection)
-
+  location = nil
+  index = 0
+  while index < collection.size
+    if yield(collection[index])
+      location = index
+      break
+    else
+      index += 1
+    end
+  end
+  if location
+    collection[location]
+  else
+    nil
+  end
 end
