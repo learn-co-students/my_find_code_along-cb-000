7309b647ef7a8f0516dceeea7e9305c8300064c2
Darnell Gillead <dlgillead@gmail.com>
2017-05-02 05:27:41 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,16 @@
 require 'pry'
 
 def my_find(collection)
+  i = 0
+  while i < collection.length
+    if yield(collection[i])
+      return collection[i]
+    end
+    i += 1
+  end
+end
 
+collection = (1..100).to_a
+my_find(collection) do |element|
+  element % 3 == 0 && element % 5 == 0
 end
