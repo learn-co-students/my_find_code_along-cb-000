6cfc5454be0509557d6b30314a3be91fc5a8d80a
Jen Poole <contact.vandepool@gmail.com>
2017-08-14 02:55:19 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,9 @@
 require 'pry'
 
 def my_find(collection)
-
+  # 1. iterate through each element in collection using while; increment by 1
+  i = 0
+  while i < collection.length
+    i += 1
+  end
 end
