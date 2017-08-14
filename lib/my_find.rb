bd695e2e4355a93a4e19441daf47674e45172579
Jen Poole <contact.vandepool@gmail.com>
2017-08-14 02:56:27 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -4,6 +4,8 @@ def my_find(collection)
   # 1. iterate through each element in collection using while; increment by 1
   i = 0
   while i < collection.length
+    # 2. for each item in the collection, yield it to the block
+    yield(collection[i])
     i += 1
   end
 end
