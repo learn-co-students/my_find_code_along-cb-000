c7b4b5b3be696782cb3eaae250f07d9481e7ba5f
adam n <adam.nawroc@gmail.com>
2018-02-28 17:56:56 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -2,11 +2,13 @@ require 'pry'
 
 def my_find(collection)
   i = 0
-  block_return_values = []
+  # block_return_values = []
   while i < collection.length
-    block_return_values << yield(collection[i])
+    if yield(collection[i])
+      return collection[i]
+    end
     i = i + 1
   end
 
-  block_return_values[0]
+  # block_return_values[0]
 end
