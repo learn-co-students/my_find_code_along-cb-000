a0f00758c833311feaa5e6589d934fcd088f885f
Mark Lindberg <mentalmegalodon@gmail.com>
2018-08-14 20:50:04 -0500

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,13 @@
 require 'pry'
 
 def my_find(collection)
-
+  index = 0
+  while index < collection.length
+    val = yield(collection[index])
+    if val
+      break
+    end
+    index += 1
+  end
+  return collection[index]
 end
