4917a732faf630cce3d5207cc6a6972cc65b57d6
Konstantinos Pediaditakis <pediadk@gmail.com>
2018-09-20 10:11:45 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,13 @@
 require 'pry'
 
 def my_find(collection)
-
+  if block_given?
+    i = 0
+    while i < collection.size
+      return collection[i] if yield(collection[i])
+      i += 1
+    end
+  else
+    "No block was provided!"
+  end
 end
