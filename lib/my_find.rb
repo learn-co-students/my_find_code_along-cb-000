41f08a0b702c69b19bd0cd72c2d5bc027c508b32
Alex Juszczakiewicz <curiositylex@protonmail.com>
2019-01-11 15:33:51 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,14 @@
 require 'pry'
 
 def my_find(collection)
-
+  if block_given?
+    i = 0 
+    while i < collection.size
+      return collection[i] if yield(collection[i]) 
+      i += 1
+    end
+    nil
+  else
+    collection
+  end
 end
